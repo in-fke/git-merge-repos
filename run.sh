4ea2d0ccfcbe3256dc95e252bb1748abb9295619
Florian Kolbe <Florian.Kolbe@gft.com>
2022-02-21 12:58:01 +0100

    moved to "java" call

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-BASEDIR=`dirname $0`
-mvn --file "$BASEDIR/pom.xml" --quiet clean compile exec:java -Dexec.args="$*"
+# BASEDIR=`dirname $0`
+# mvn --file "$BASEDIR/pom.xml" --quiet clean compile exec:java -Dexec.args="$*"
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+(
+cd "$SCRIPT_PATH" && \
+mvn clean install && \
+java -Xmx512m -cp '$SCRIPT_PATH/lib/*' org.nibor.git_merge_repos.Main "$@"
+)
